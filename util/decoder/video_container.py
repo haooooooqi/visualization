e967ae34a186ae5bd8299c528baeaee22e569e25
Haoqi Fan <3500423+haooooooqi@users.noreply.github.com>
2022-04-23 11:50:57 -0700

    Update video_container.py

--- util/decoder/video_container.py
+++ util/decoder/video_container.py
@@ -6,11 +6,8 @@ import sys
 import tempfile
 from io import BytesIO
 
-import common.thread_safe_fork
 import pycurl
 import ti.urlgen.everstore_url_py as everstore_url_py
-
-common.thread_safe_fork.threadSafeForkRegisterAtFork()
 
 
 def get_video_container(handle, multi_thread_decode=False):
